040ebec133a56603683c56a36e8407cc02fde5be
RITHIK KUMARAN K <rithikkumarank@gmail.com>
2025-03-27 20:27:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install --upgrade pip
-apt-get update
-apt-get install -y libgl1-mesa-glx libglib2.0-0
-
+pip cache purge
+pip install --force-reinstall transformers==4.49.0
+pip install -r requirements.txt
